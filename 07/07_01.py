741bf204bc88d558e09e7468e958907059891028
Louis-Émile Robitaille <emile.courriel@gmail.com>
2022-12-07 08:21:54 -0500

    Add day_07 of the advent of code

---
+++ 07/07_01.py
@@ -0,0 +1,39 @@
+from collections import Counter
+from typing import Optional
+
+
+def execute_cd(location_instr: str, location: list):
+    if location_instr == "/":
+        location.clear()
+        location.append("/")
+    elif location_instr == "..":
+        location.pop()
+    else:
+        old_location = "" if location[-1] == "/" else location[-1]
+        new_location = "/".join([old_location, location_instr])
+        location.append(new_location)
+
+
+def execute_command(command: list, location: list):
+    match command[0]:
+        case "cd":
+            execute_cd(command[1], location)
+        case "ls":
+            pass
+
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as f:
+        location = ["/"]
+        dirs = Counter()
+        for line in f.readlines():
+            line = line.strip("\n")
+            parts = line.split(" ")
+            if parts[0] == "$":
+                execute_command(parts[1:], location)
+            else:
+                if parts[0] != "dir":
+                    size, filename = parts
+                    for dir in location:
+                        dirs[dir] += int(size)
+    print(sum(filter(lambda x: x <= 100000, dirs.values())))
